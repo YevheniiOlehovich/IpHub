28533b705c50c1ec272d519e3b7e5398581bb559
Yevhenii Khyzhniak <keildra258@gmail.com>
2025-01-22 23:08:35 +0200

    finished purpose section

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,3 @@
-/**
- * !(i)
- * Код попадает в итоговый файл, только когда вызвана функция, например FLSFunctions.spollers();
- * Или когда импортирован весь файл, например import "files/script.js";
- * Неиспользуемый код в итоговый файл не попадает.
-
- * Если мы хотим добавить модуль следует его раскомментировать
- */
-// import { MousePRLX } from './libs/parallaxMouse'
-// import AOS from 'aos'
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 import { BaseHelpers } from './helpers/base-helpers';
@@ -25,45 +15,6 @@ BaseHelpers.addTouchClass();
 BaseHelpers.addLoadedClass();
 
 BaseHelpers.headerFixed();
-
-/**
- * Открытие/закрытие модальных окон
- * Чтобы модальное окно открывалось и закрывалось
- * На окно повешай атрибут data-popup="<название окна>"
- * На кнопку, которая вызывает окно повешай атрибут data-type="<название окна>"
-
- * На обертку(.popup) окна добавь атрибут '[data-close-overlay]'
- * На кнопку для закрытия окна добавь класс '.button-close'
- * */
-new PopupManager();
-
-/**
- *  Модуль для работы с меню (Бургер)
- * */
-new BurgerMenu().init();
-
-/**
- *  Библиотека для анимаций
- *  документация: https://michalsnik.github.io/aos
- * */
-// AOS.init();
-
-/**
- * Параллакс мышей
- * */
-// new MousePRLX();
-
-// new Tabs('tabs-example', {
-// 	onChange: (data) => {
-// 		console.log(data);
-// 	},
-// });
-
-// new Accordion('.accordion', {
-// 	shouldOpenAll: false, // true
-// 	defaultOpen: [], // [0,1]
-// 	collapsedClass: 'open',
-// });
 
 const burgerButton = document.querySelector('.js-button')
 const dropMenu = document.querySelector('.js-menu')
@@ -127,8 +78,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 const currentSlide = document.querySelector('.current-slide');
                 const totalSlides = document.querySelector('.total-slides');
 
-                currentSlide.textContent = this.realIndex + 1; // Поточний слайд
-                totalSlides.textContent = this.slides.length; // Загальна кількість слайдів (без врахування слайдів для навігації)
+                currentSlide.textContent = this.realIndex + 1; 
+                totalSlides.textContent = this.slides.length; 
 
                 if (this.isBeginning) {
                     prevButton.style.display = 'none';
@@ -187,9 +138,7 @@ function renderSlides(containerSelector) {
                 input.value = labelText;
 
                 input.addEventListener('change', () => {
-                    console.log(input.value);
                     selectedValues[`slide-${index}`] = input.value;
-                    console.log('Selected values:', selectedValues);
 
                     const labels = labelsBox.querySelectorAll('.purpose__swiper-item-label');
                     labels.forEach((lbl) => lbl.classList.remove('active-label'));
@@ -212,37 +161,21 @@ renderSlides('.purpose__swiper-wrapper');
 
 
 
-const YOUR_SERVICE_ID = 'service_nf3duct'
-const YOUR_TEMPLATE_ID = 'NRbTukRmATT-SxuhU'
-const YOUR_USER_ID = 'FCxwI9uRYiKRbqCz5ow1S'
+function sendEmail(){
 
-
-import emailjs from 'emailjs-com';
-
-document.querySelector('.send-button').addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log('send')
-    const name = document.getElementById('contact-name').value;
-    const email = document.getElementById('contact-email').value;
-    const message = document.getElementById('contact-textarea').value;
-
-    if (name && email) {
-        const templateParams = {
-            from_name: name,
-            from_email: email,
-            message: message
-        };
-
-        // Використовуємо EmailJS для відправки
-        emailjs.send(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, templateParams, YOUR_USER_ID)
-            .then((response) => {
-                console.log('SUCCESS!', response);
-                alert('Повідомлення надіслано!');
-            }, (error) => {
-                console.log('FAILED...', error);
-                alert('Сталася помилка при відправці.');
-            });
-    } else {
-        alert('Будь ласка, заповніть всі поля.');
+    let params = {
+        from_name: document.getElementById('contact-name').value,
+        email: document.getElementById('contact-email').value,
+        message: document.getElementById('contact-textarea').value,
     }
-});
+    emailjs.send("service_nf3duct","template_uilbwd1", params)
+    .then(function(){
+        alert('Succsesfully send')
+    })
+}
+
+const sendButton = document.querySelector('.send-button');
+
+sendButton.onclick = () => {
+    sendEmail();
+}
